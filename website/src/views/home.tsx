7eec3b7f04451d0d320a215fefbb5e3462d88497
donghainan <hainan.dong@autostreets.com>
2020-07-20 13:16:43 +0800

    前端页面

--- website/src/views/home.tsx
+++ website/src/views/home.tsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { Layout, Menu } from 'antd'
 import {
-	UserOutlined,
-	PieChartOutlined,
-	ThunderboltTwoTone
+	ThunderboltTwoTone,
+	Html5TwoTone,
+	InteractionTwoTone,
+	FireTwoTone,
+	LikeTwoTone,
+	DatabaseTwoTone,
+	AppstoreTwoTone,
+	BugTwoTone,
+	CustomerServiceTwoTone,
+	HeartTwoTone,
+	RocketTwoTone,
+	TrophyTwoTone,
+	CopyrightCircleTwoTone,
 } from '@ant-design/icons'
-
 const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
 import '../styles/home.less'
@@ -18,24 +27,97 @@ const Home = () => {
 					<div className="logo">
 						<span>Dong's Blog</span>
 					</div>
-					<div className="blog-title"><ThunderboltTwoTone />天下技术、唯勤不破<ThunderboltTwoTone /></div>
+					<div className="blog-title">
+						<ThunderboltTwoTone />
+						&nbsp; 天下技术、唯勤不破 &nbsp;
+						<ThunderboltTwoTone />
+					</div>
 				</Header>
 				<Layout>
 					<Sider width={200} className="site-layout-background">
 						<Menu
 							mode="inline"
-							defaultSelectedKeys={['1']}
-							defaultOpenKeys={['sub1']}
-							style={{ height: '100%', borderRight: 0 }}
+							defaultSelectedKeys={['html-1']}
+							defaultOpenKeys={['html']}
+							style={{
+								borderRight: 0,
+								height: '100%',
+							}}
 						>
-							<Menu.Item key="0" icon={<PieChartOutlined />}>
-								Option 1
-							</Menu.Item>
-							<SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-								<Menu.Item key="1">option1</Menu.Item>
-								<Menu.Item key="2">option2</Menu.Item>
-								<Menu.Item key="3">option3</Menu.Item>
-								<Menu.Item key="4">option4</Menu.Item>
+							<SubMenu key="html" icon={<Html5TwoTone />} title="Html">
+								<Menu.Item key="html-1">option1</Menu.Item>
+							</SubMenu>
+							<SubMenu key="css" icon={<CopyrightCircleTwoTone />} title="Css">
+								<Menu.Item key="css-1">option1</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="javascript"
+								icon={<TrophyTwoTone />}
+								title="Javascript"
+							>
+								<Menu.Item key="js-1">option1</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="typescript"
+								icon={<RocketTwoTone />}
+								title="Typescript"
+							>
+								<Menu.Item key="typescript-1">option1</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="网络请求"
+								icon={<InteractionTwoTone />}
+								title="网络请求"
+							>
+								<Menu.Item key="网络请求-XMLHttpRequest">
+									XMLHttpRequest
+								</Menu.Item>
+								<Menu.Item key="网络请求-ajax">Ajax</Menu.Item>
+								<Menu.Item key="网络请求-fetch">Fetch</Menu.Item>
+								<Menu.Item key="网络请求-promise">Promise</Menu.Item>
+							</SubMenu>
+							<SubMenu key="热门框架" icon={<FireTwoTone />} title="热门框架">
+								<Menu.Item key="热门框架-vue">Vue</Menu.Item>
+								<Menu.Item key="热门框架-react">React</Menu.Item>
+								<Menu.Item key="热门框架-angular">Angular</Menu.Item>
+								<Menu.Item key="热门框架-singleSpa">前端微服务</Menu.Item>
+							</SubMenu>
+							<SubMenu key="跨端技术" icon={<LikeTwoTone />} title="跨端技术">
+								<Menu.Item key="跨端技术-ReactNative">ReactNative</Menu.Item>
+								<Menu.Item key="跨端技术-Flutter">Flutter</Menu.Item>
+								<Menu.Item key="跨端技术-Weex">Weex</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="后端技术"
+								icon={<DatabaseTwoTone />}
+								title="后端技术"
+							>
+								<Menu.Item key="后端技术-Node">Node</Menu.Item>
+								<Menu.Item key="后端技术-Eggjs">Eggjs</Menu.Item>
+								<Menu.Item key="后端技术-Deno">Deno</Menu.Item>
+								<Menu.Item key="后端技术-Python">Python</Menu.Item>
+								<Menu.Item key="后端技术-Php">Php</Menu.Item>
+								<Menu.Item key="后端技术-Java">Java</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="桌面应用"
+								icon={<AppstoreTwoTone />}
+								title="桌面应用"
+							>
+								<Menu.Item key="桌面应用-Electron">Electron</Menu.Item>
+							</SubMenu>
+							<SubMenu key="网络通信" icon={<BugTwoTone />} title="网络通信">
+								<Menu.Item key="网络通信-1">HTTP&HTTPS</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="前端杂谈"
+								icon={<CustomerServiceTwoTone />}
+								title="前端杂谈"
+							>
+								<Menu.Item key="前端杂谈-1">前端杂谈</Menu.Item>
+							</SubMenu>
+							<SubMenu key="放飞自我" icon={<HeartTwoTone />} title="放飞自我">
+								<Menu.Item key="放飞自我-1">放飞自我</Menu.Item>
 							</SubMenu>
 						</Menu>
 					</Sider>
